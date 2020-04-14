d694a76762329094fd77b11c16b2ad4d1e786d76
patrickjuniore <54557518+patrickjuniore@users.noreply.github.com>
2020-04-14 08:36:56 +0200

    Create defi0.js

---
+++ 1.Fondamentaux/defi0.js
@@ -0,0 +1,47 @@
+const https = require('https');
+const url = "https://blockchain.info/rawblock/000000000002de92d93fcb92eeb2be097af8570a70fa5a8c6df473626891c9d6"
+
+
+const req = https.get(url, (res) => {
+  // variable vide
+  let resultat = '';
+
+  res.on('data', (data) => {
+    resultat += data;
+  });
+
+  //afficher le resulat
+  //const obj = JSON.parse(resultat).hash
+
+  res.on('end', () => {
+    console.log(JSON.parse(resultat).time);
+  });
+
+  res.on('end', () => {
+    console.log(JSON.parse(resultat).height);
+  });
+
+  res.on('end', () => {
+    console.log(JSON.parse(resultat).hash);
+  });
+
+
+  /*res.on('data', d => {
+    process.stdout.write(d)
+  })*/
+
+});
+
+//req.write(data)
+
+req.on('error', (e) => {
+    console.error(e);
+  });
+
+//afficher le resulat
+
+//console.log(req);
+//console.log(JSON.parse(req.resultat).hash);
+
+
+req.end();
