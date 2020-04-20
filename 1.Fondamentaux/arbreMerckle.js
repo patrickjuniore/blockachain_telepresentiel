f16872c804eefee332f8c83987a0b96932204133
patrickjuniore <54557518+patrickjuniore@users.noreply.github.com>
2020-04-20 02:47:59 +0200

    Update arbreMerckle.js

--- 1.Fondamentaux/arbreMerckle.js
+++ 1.Fondamentaux/arbreMerckle.js
@@ -7,37 +7,44 @@ class premiereLigne {
       this.premiereLigne  = premiereLigne
     }  
 
-    //hashPremiereLigne(input) {
-    hashPremiereLigne() {
-        let hashPremiereLigne = []
-        //var inputUser = input
-        var inputUser = ["lapin","carotte","champs","arbre"]
-        for(var  i= 0; i < x.length; i++) {
-            data = Buffer.from(inputUser[i])
-            hash = crypto.createHash("sha256").update(data).digest() 
-            hashPremiereLigne.push(this.hash)
-        }
-        return hashPremiereLigne
-    }
 }
 
 class Arbre {
     constructor(inputUser) {
         this.inputUser = ["lapin","carotte","champs","arbre"]
-        this.hashPremiereLigne = []
+        //this.hashPremiereLigne = []
         //this.premiereLigne = new premiereLigne(premiereLigne)
     }  
 
-    hashPremiereLigne(){
-        if (this.inputUser !== undefined)
-            return this.hashPremiereLigne.hashPremiereLigne()
-        return []
-    }
+    //hashPremiereLigne(input) {
+    hashPremiereLigne() {
+        let hashPremiereLigne = []
+        const crypto = require ("crypto") 
+        //var inputUser = input
+        var inputUser = ["lapin","carotte","champs","arbre"]
+        for(var  i= 0; i < inputUser.length; i++) {
+            let data = Buffer.from(inputUser[i])
+            let hash = crypto.createHash("sha256").update(data).digest("hex") 
+            hashPremiereLigne.push(hash)
+        }
+        return hashPremiereLigne
+    }    
+
+    buildMerckle() {
+        let hashPremiereLigne = []
+        const crypto = require ("crypto") 
+        //var inputUser = input
+        var inputUser = this.hashPremiereLigne
+        for(var  i= 0; i < inputUser.length; i++) {
+            let data = Buffer.from(inputUser[i])
+            let hash = crypto.createHash("sha256").update(data).digest("hex") 
+            hashPremiereLigne.push(hash)
+        }
+        return hashPremiereLigne
+    } 
+
 }
 
 let monArbre = new Arbre(1)
 console.log(monArbre.inputUser[0])
-// console.log(monArbre.hashPremiereLigne())
-
-//let mapremiereLigne = new premiereLigne(1)
-//console.log(mapremiereLigne.premiereLigne[0])
+console.log(monArbre.hashPremiereLigne())
